506e74306684b628d8714d3359bfccef5f16f506
Stanislaw A. Zhukov <stanislaw.zhukov@yandex.ru>
2012-11-18 20:40:52 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dz4::Application.config.secret_token = 'd10e08d8e81a9368d478f285e6bbc5a054553d922496782dfaea0cbfe8b8fbbd4ca1c0b1cc0676214b0841695c46ac20ece687d14d5810ccd539f35dbb94bee4'
